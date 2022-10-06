2a84409cfa6befa0af2ea161d389262a3afe755f
Alexander Andreev <alexander.andreev@intel.com>
2022-10-06 20:17:09 +0100

    Update params variants; Add RF regression

--- Program.cs
+++ Program.cs
@@ -9,9 +9,9 @@ using Newtonsoft.Json;
 
 namespace mlnet_bench
 {
-	class Program
+    class Program
     {
-    	public static IDataView[] LoadData(
+        public static IDataView[] LoadData(
             MLContext mlContext, string trainingFile, string testingFile,
             string task, string label = "target", char separator = ',')
         {
@@ -61,7 +61,7 @@ namespace mlnet_bench
             return featuresList.ToArray();
         }
 
-        public static double[] RunRandomForestClassifier(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName, int numberOfTrees, int numberOfLeaves)
+        public static double[] RunRandomForestClassification(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName, int numberOfTrees, int numberOfLeaves)
         {
             var featuresArray = GetFeaturesArray(trainingData, labelName);
             var preprocessingPipeline = mlContext.Transforms.Concatenate("Features", featuresArray);
@@ -93,9 +93,41 @@ namespace mlnet_bench
             return metrics;
         }
 
-    	static void Main(string[] args)
+        public static double[] RunRandomForestRegression(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName, int numberOfTrees, int numberOfLeaves)
         {
-        	// args[0] - training data filename
+            var featuresArray = GetFeaturesArray(trainingData, labelName);
+            var preprocessingPipeline = mlContext.Transforms.Concatenate("Features", featuresArray);
+            var preprocessedTrainingData = preprocessingPipeline.Fit(trainingData).Transform(trainingData);
+            var preprocessedTestingData = preprocessingPipeline.Fit(trainingData).Transform(testingData);
+
+            FastForestRegressionTrainer.Options options = new FastForestRegressionTrainer.Options();
+            options.LabelColumnName = labelName;
+            options.FeatureColumnName = "Features";
+            options.NumberOfTrees = numberOfTrees;
+            options.NumberOfLeaves = numberOfLeaves;
+            options.MinimumExampleCountPerLeaf = 5;
+            options.FeatureFraction = 1.0;
+
+            var trainer = mlContext.Regression.Trainers.FastForest(options);
+
+            ITransformer model = trainer.Fit(preprocessedTrainingData);
+
+            IDataView trainingPredictions = model.Transform(preprocessedTrainingData);
+            var trainingMetrics = mlContext.Regression.Evaluate(trainingPredictions, labelColumnName: labelName);
+            IDataView testingPredictions = model.Transform(preprocessedTestingData);
+            var testingMetrics = mlContext.Regression.Evaluate(testingPredictions, labelColumnName: labelName);
+
+            double[] metrics = new double[4];
+            metrics[0] = trainingMetrics.RootMeanSquaredError;
+            metrics[1] = testingMetrics.RootMeanSquaredError;
+            metrics[2] = trainingMetrics.RSquared;
+            metrics[3] = testingMetrics.RSquared;
+            return metrics;
+        }
+
+        static void Main(string[] args)
+        {
+            // args[0] - training data filename
             // args[1] - testing data filename
             // args[2] - machine learning task (regression/binary)
             // Random Forest parameters:
@@ -107,18 +139,24 @@ namespace mlnet_bench
             IDataView[] data = LoadData(mlContext, args[0], args[1], args[2]);
             string labelName = "target";
 
-        	var mainWatch = System.Diagnostics.Stopwatch.StartNew();
+            var mainWatch = System.Diagnostics.Stopwatch.StartNew();
             double[] metrics;
+            int numberOfTrees = Int32.Parse(args[3]);
+            int numberOfLeaves = Int32.Parse(args[4]);
             if (args[2] == "binary")
             {
-                int numberOfTrees = Int32.Parse(args[3]);
-                int numberOfLeaves = Int32.Parse(args[4]);
-                metrics = RunRandomForestClassifier(mlContext, data[0], data[1], labelName, numberOfTrees, numberOfLeaves);
+                metrics = RunRandomForestClassification(mlContext, data[0], data[1], labelName, numberOfTrees, numberOfLeaves);
                 mainWatch.Stop();
                 Console.WriteLine("algorithm,all workflow time[ms],training accuracy,testing accuracy,training F1 score,testing F1 score");
                 Console.WriteLine($"Random Forest Classification,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
             }
-
+            else
+            {
+                metrics = RunRandomForestRegression(mlContext, data[0], data[1], labelName, numberOfTrees, numberOfLeaves);
+                mainWatch.Stop();
+                Console.WriteLine("algorithm,all workflow time[ms],training RMSE,testing RMSE,training R2 score,testing R2 score");
+                Console.WriteLine($"Random Forest Regression,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
+            }
         }
     }
 }
--- run_bench.py
+++ run_bench.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_classification, make_regression
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 import subprocess
+import sys
 import os
 import io
+
+
+RANDOM_STATE = 42
+np.random.seed(RANDOM_STATE)
 
 
 def convert_to_csv(data, filename, label='target'):
@@ -20,63 +24,89 @@ def read_output_from_command(command, env):
     return res.stdout[:-1], res.stderr[:-1]
 
 
-random_state = 42
-np.random.seed(42)
+def run_case(n_samples, n_features, n_trees, n_leaves, task_type):
+    data_params = {
+        'n_samples': 2 * n_samples,
+        'n_features': n_features,
+        'n_informative': n_features // 2,
+        'random_state': RANDOM_STATE
+    }
+    if task_type == 'binary':
+        x, y = make_classification(**data_params)
+    elif task_type == 'regression':
+        x, y = make_regression(**data_params)
 
-n_samples_range = [10000, 20000]
-n_features_range = [8, 32]
-n_trees_range = [50, 200]
-n_leaves_range = [64, 256]
+    data = np.concatenate([x, y.reshape(-1, 1)], axis=1)
+    train_data, test_data = train_test_split(data, test_size=0.5, random_state=RANDOM_STATE)
 
-default_result = None
-optimized_result = None
+    train_filename, test_filename = "synth_data_train.csv", "synth_data_test.csv"
+    convert_to_csv(train_data, train_filename)
+    convert_to_csv(test_data, test_filename)
+
+    print(f'n_samples={n_samples}, n_features={n_features}, n_trees={n_trees}, n_leaves={n_leaves}')
+
+    env_copy = os.environ.copy()
+    default_stdout, default_stderr = read_output_from_command(f'dotnet run {train_filename} {test_filename} {task_type} {n_trees} {n_leaves}', env_copy)
+
+    print('DEFAULT STDOUT:', default_stdout, 'DEFAULT STDERR:', default_stderr, sep='\n')
+
+    default_res = io.StringIO(default_stdout + '\n')
+
+    default_res = pd.DataFrame(
+        {'algorithm': [f'Random Forest {task_type.capitalize()}'],
+        'n samples': [n_samples], 'n features': [n_features],
+        'n trees': [n_trees], 'n leaves': [n_leaves]}).merge(pd.read_csv(default_res))
+
+    env_copy['MLNET_BACKEND'] = 'ONEDAL'
+    optimized_stdout, optimized_stderr = read_output_from_command(f'dotnet run {train_filename} {test_filename} {task_type} {n_trees} {n_leaves}', env_copy)
+
+    print('OPTIMIZED STDOUT:', optimized_stdout, 'OPTIMIZED STDERR:', optimized_stderr, sep='\n')
+
+    optimized_res = io.StringIO(optimized_stdout + '\n')
+    optimized_res = pd.read_csv(optimized_res)
+
+    if task_type == 'binary':
+        metrics = ['accuracy', 'F1 score']
+    elif task_type == 'regression':
+        metrics = ['RMSE', 'R2 score']
+    metrics = ['all workflow time[ms]', f'training {metrics[0]}', f'testing {metrics[0]}', f'training {metrics[1]}', f'testing {metrics[1]}']
+    metrics_map_template = {el: '{} ' + f'{el}' for el in metrics}
+
+    default_res = default_res.rename(columns={k: v.format('ML.NET') for k, v in metrics_map_template.items()})
+    optimized_res = optimized_res.rename(columns={k: v.format('oneDAL') for k, v in metrics_map_template.items()})
+
+    result = default_res.merge(optimized_res)
+
+    return result
+
+
+n_samples_range = [10000, 20000, 50000]
+n_features_range = [8, 32, 64]
+n_trees_range = [50, 100, 200]
+n_leaves_range = [64, 128, 256]
+# n_samples_range = [10000]
+# n_features_range = [8, 16]
+# n_trees_range = [50]
+# n_leaves_range = [64]
+n_runs = 5
+
+result = None
 for n_samples in n_samples_range:
     for n_features in n_features_range:
         for n_trees in n_trees_range:
             for n_leaves in n_leaves_range:
-                x, y = make_classification(n_samples=2 * n_samples, n_features=n_features, n_informative=n_features // 2, random_state=random_state)
-                data = np.concatenate([x, y.reshape(-1, 1)], axis=1)
-                train_data, test_data = train_test_split(data, test_size=0.5, random_state=random_state)
+                for i in range(n_runs):
+                    new_result = run_case(n_samples, n_features, n_trees, n_leaves, task_type='binary')
+                    if result is None:
+                        result = new_result
+                    else:
+                        result = pd.concat([result, new_result], axis=0)
 
-                train_filename, test_filename = "synth_data_bin_train.csv", "synth_data_bin_test.csv"
-                convert_to_csv(train_data, train_filename)
-                convert_to_csv(test_data, test_filename)
+all_columns = list(result.columns)
+groupby_columns = ['algorithm', 'n samples', 'n features', 'n trees', 'n leaves']
+metric_columns = list(result.columns)
+for column in groupby_columns:
+    metric_columns.remove(column)
 
-                env_copy = os.environ.copy()
-                default_stdout, default_stderr = read_output_from_command(f'dotnet run {train_filename} {test_filename} binary {n_trees} {n_leaves}', env_copy)
-
-                print(f'n_samples={n_samples}, n_features={n_features}, n_trees={n_trees}, n_leaves={n_leaves}')
-                print(default_stdout)
-                print(default_stderr)
-                default_res = io.StringIO(default_stdout + '\n')
-                default_res = pd.read_csv(default_res) # .rename(columns={'all workflow time[ms]', 'training accuracy', 'testing accuracy', 'training F1 score', 'testing F1 score'})
-                default_res['n samples'] = np.array([n_samples, ])
-                default_res['n features'] = np.array([n_features, ])
-                default_res['n trees'] = np.array([n_trees, ])
-                default_res['n leaves'] = np.array([n_leaves, ])
-                if default_result is None:
-                    default_result = default_res.copy()
-                else:
-                    default_result = pd.concat([default_result, default_res], axis=0)
-
-                env_copy['MLNET_BACKEND'] = 'ONEDAL'
-                optimized_stdout, optimized_stderr = read_output_from_command(f'dotnet run {train_filename} {test_filename} binary {n_trees} {n_leaves}', env_copy)
-
-                print(optimized_stdout)
-                print(optimized_stderr)
-                optimized_res = io.StringIO(optimized_stdout + '\n')
-                optimized_res = pd.read_csv(optimized_res)
-                optimized_res['n samples'] = np.array([n_samples, ])
-                optimized_res['n features'] = np.array([n_features, ])
-                optimized_res['n trees'] = np.array([n_trees, ])
-                optimized_res['n leaves'] = np.array([n_leaves, ])
-                if optimized_result is None:
-                    optimized_result = optimized_res.copy()
-                else:
-                    optimized_result = pd.concat([optimized_result, optimized_res], axis=0)
-
-default_result = default_result.rename(columns={el: f'ML.NET {el}' for el in ['all workflow time[ms]', 'training accuracy', 'testing accuracy', 'training F1 score', 'testing F1 score']})
-optimized_result = optimized_result.rename(columns={el: f'oneDAL {el}' for el in ['all workflow time[ms]', 'training accuracy', 'testing accuracy', 'training F1 score', 'testing F1 score']})
-
-default_result.to_csv('default_result.csv', index=False)
-optimized_result.to_csv('optimized_result.csv', index=False)
+result = result.groupby(groupby_columns)[metric_columns].mean().reset_index()
+result.to_csv('result.csv', index=False)
