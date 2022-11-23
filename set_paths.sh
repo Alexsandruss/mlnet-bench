749ae7a4db2e40d670f3c4749dffe461ac3d99ef
Alexander Andreev <alexander.andreev@intel.com>
2022-11-23 11:38:04 +0000

    Temp.fix for linking on macos

--- set_paths.sh
+++ set_paths.sh
@@ -8,6 +8,10 @@ elif [ ${os} = "Darwin" ]; then
     export DYLD_LIBRARY_PATH=$PWD/../machinelearning/artifacts/bin/Native/x64.Debug:$DYLD_LIBRARY_PATH
     export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
     export DYLD_LIBRARY_PATH=$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH
+    # TODO: fix linking on MacOS
+    ln -s libOneDalNative.dylib ../machinelearning/artifacts/bin/Native/x64.Debug/libOneDalNative
+    ln -s libonedal_core.1.1.dylib ../machinelearning/artifacts/bin/Native/x64.Debug/libonedal_core.dylib
+    ln -s libonedal_thread.1.1.dylib ../machinelearning/artifacts/bin/Native/x64.Debug/libonedal_thread.dylib
 else
     echo "Wrong system: ${os}"
     exit 1
