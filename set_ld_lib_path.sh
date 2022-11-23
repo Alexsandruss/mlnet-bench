6110a0a3647edc299b4bedaccc244fe88b6ef8ce
Alexander Andreev <alexander.andreev@intel.com>
2022-11-23 10:10:28 +0000

    Update paths script

--- set_ld_lib_path.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-export LD_LIBRARY_PATH=$PWD/../machinelearning/artifacts/bin/Native/x64.Debug:$LD_LIBRARY_PATH
---
+++ set_paths.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+os=$(uname)
+if [ ${os} = "Linux" ]; then
+    export LD_LIBRARY_PATH=$PWD/../machinelearning/artifacts/bin/Native/x64.Debug:$LD_LIBRARY_PATH
+elif [ ${os} = "Darwin" ]; then
+    export LIBRARY_PATH=$PWD/../machinelearning/artifacts/bin/Native/x64.Debug:$LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$PWD/../machinelearning/artifacts/bin/Native/x64.Debug:$DYLD_LIBRARY_PATH
+    export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
+    export DYLD_LIBRARY_PATH=$CONDA_PREFIX/lib:$DYLD_LIBRARY_PATH
+else
+    echo "Wrong system: ${os}"
+    exit 1
+fi
+export CPATH=$CONDA_PREFIX/include:$CPATH
