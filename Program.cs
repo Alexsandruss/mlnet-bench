43dcb1fe422f4e33533bfef68cfd4cc97a74ce20
Alexander Andreev <alexander.andreev@intel.com>
2023-02-14 13:10:04 +0000

    Update benchmark

--- Program.cs
+++ Program.cs
@@ -61,28 +61,28 @@ namespace mlnet_bench
             return featuresList.ToArray();
         }
 
-        public static double[] RunRandomForestClassification(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName, int numberOfTrees, int numberOfLeaves)
+        public static IDataView[] PreprocessData(MLContext mlContext, string trainingFile, string testingFile,
+            string task, string labelName, string featuresName = "Features")
         {
-            var featuresArray = GetFeaturesArray(trainingData, labelName);
-            var preprocessingPipeline = mlContext.Transforms.Concatenate("Features", featuresArray);
-            var preprocessedTrainingData = preprocessingPipeline.Fit(trainingData).Transform(trainingData);
-            var preprocessedTestingData = preprocessingPipeline.Fit(trainingData).Transform(testingData);
+            IDataView[] data = LoadData(mlContext, trainingFile, testingFile, task, labelName);
+            string[] featuresArray = GetFeaturesArray(data[0], labelName);
+            var preprocessingPipeline = mlContext.Transforms.Concatenate(featuresName, featuresArray);
+            var pipeline = preprocessingPipeline.Fit(data[0]);
+            var preprocessedTrainingData = pipeline.Transform(data[0]);
+            var preprocessedTestingData = pipeline.Transform(data[1]);
 
-            FastForestBinaryTrainer.Options options = new FastForestBinaryTrainer.Options();
-            options.LabelColumnName = labelName;
-            options.FeatureColumnName = "Features";
-            options.NumberOfTrees = numberOfTrees;
-            options.NumberOfLeaves = numberOfLeaves;
-            options.MinimumExampleCountPerLeaf = 5;
-            options.FeatureFraction = 1.0;
+            List<IDataView> dataList = new List<IDataView>();
+            dataList.Add(preprocessedTrainingData);
+            dataList.Add(preprocessedTestingData);
 
-            var trainer = mlContext.BinaryClassification.Trainers.FastForest(options);
+            return dataList.ToArray();
+        }
 
-            ITransformer model = trainer.Fit(preprocessedTrainingData);
-
-            IDataView trainingPredictions = model.Transform(preprocessedTrainingData);
+        public static double[] EvaluateBinaryClassification(MLContext mlContext, ITransformer model, IDataView trainingData, IDataView testingData, string labelName)
+        {
+            IDataView trainingPredictions = model.Transform(trainingData);
             var trainingMetrics = mlContext.BinaryClassification.EvaluateNonCalibrated(trainingPredictions, labelColumnName: labelName);
-            IDataView testingPredictions = model.Transform(preprocessedTestingData);
+            IDataView testingPredictions = model.Transform(testingData);
             var testingMetrics = mlContext.BinaryClassification.EvaluateNonCalibrated(testingPredictions, labelColumnName: labelName);
 
             double[] metrics = new double[4];
@@ -93,16 +93,61 @@ namespace mlnet_bench
             return metrics;
         }
 
-        public static double[] RunRandomForestRegression(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName, int numberOfTrees, int numberOfLeaves)
+        public static double[] EvaluateRegression(MLContext mlContext, ITransformer model, IDataView trainingData, IDataView testingData, string labelName)
         {
-            var featuresArray = GetFeaturesArray(trainingData, labelName);
-            var preprocessingPipeline = mlContext.Transforms.Concatenate("Features", featuresArray);
-            var preprocessedTrainingData = preprocessingPipeline.Fit(trainingData).Transform(trainingData);
-            var preprocessedTestingData = preprocessingPipeline.Fit(trainingData).Transform(testingData);
+            IDataView trainingPredictions = model.Transform(trainingData);
+            var trainingMetrics = mlContext.Regression.Evaluate(trainingPredictions, labelColumnName: labelName);
+            IDataView testingPredictions = model.Transform(testingData);
+            var testingMetrics = mlContext.Regression.Evaluate(testingPredictions, labelColumnName: labelName);
 
+            double[] metrics = new double[4];
+            metrics[0] = trainingMetrics.RootMeanSquaredError;
+            metrics[1] = testingMetrics.RootMeanSquaredError;
+            metrics[2] = trainingMetrics.RSquared;
+            metrics[3] = testingMetrics.RSquared;
+            return metrics;
+        }
+
+        public static double[] RunRandomForestClassification(MLContext mlContext, IDataView trainingData, IDataView testingData,
+            int numberOfTrees, int numberOfLeaves, string labelName, string featuresName = "Features")
+        {
+            FastForestBinaryTrainer.Options options = new FastForestBinaryTrainer.Options();
+            options.LabelColumnName = labelName;
+            options.FeatureColumnName = featuresName;
+            options.NumberOfTrees = numberOfTrees;
+            options.NumberOfLeaves = numberOfLeaves;
+            options.MinimumExampleCountPerLeaf = 5;
+            options.FeatureFraction = 1.0;
+
+            var trainer = mlContext.BinaryClassification.Trainers.FastForest(options);
+
+            ITransformer model = trainer.Fit(trainingData);
+
+            return EvaluateBinaryClassification(mlContext, model, trainingData, testingData, labelName);
+        }
+
+        public static double[] RunLogisticRegression(MLContext mlContext, IDataView trainingData, IDataView testingData,
+            int numberOfIterations, string labelName, string featuresName = "Features")
+        {
+            var options = new LbfgsLogisticRegressionBinaryTrainer.Options();
+            options.LabelColumnName = labelName;
+            options.MaximumNumberOfIterations = numberOfIterations;
+            options.L1Regularization = 0.01f;
+            options.L2Regularization = 0.01f;
+
+            var trainer = mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression(options);
+
+            ITransformer model = trainer.Fit(trainingData);
+
+            return EvaluateBinaryClassification(mlContext, model, trainingData, testingData, labelName);
+        }
+
+        public static double[] RunRandomForestRegression(MLContext mlContext, IDataView trainingData, IDataView testingData,
+            int numberOfTrees, int numberOfLeaves, string labelName, string featuresName = "Features")
+        {
             FastForestRegressionTrainer.Options options = new FastForestRegressionTrainer.Options();
             options.LabelColumnName = labelName;
-            options.FeatureColumnName = "Features";
+            options.FeatureColumnName = featuresName;
             options.NumberOfTrees = numberOfTrees;
             options.NumberOfLeaves = numberOfLeaves;
             options.MinimumExampleCountPerLeaf = 5;
@@ -110,92 +155,80 @@ namespace mlnet_bench
 
             var trainer = mlContext.Regression.Trainers.FastForest(options);
 
-            ITransformer model = trainer.Fit(preprocessedTrainingData);
+            ITransformer model = trainer.Fit(trainingData);
 
-            IDataView trainingPredictions = model.Transform(preprocessedTrainingData);
-            var trainingMetrics = mlContext.Regression.Evaluate(trainingPredictions, labelColumnName: labelName);
-            IDataView testingPredictions = model.Transform(preprocessedTestingData);
-            var testingMetrics = mlContext.Regression.Evaluate(testingPredictions, labelColumnName: labelName);
-
-            double[] metrics = new double[4];
-            metrics[0] = trainingMetrics.RootMeanSquaredError;
-            metrics[1] = testingMetrics.RootMeanSquaredError;
-            metrics[2] = trainingMetrics.RSquared;
-            metrics[3] = testingMetrics.RSquared;
-            return metrics;
+            return EvaluateRegression(mlContext, model, trainingData, testingData, labelName);
         }
 
-        public static double[] RunOLSRegression(MLContext mlContext, IDataView trainingData, IDataView testingData, string labelName)
+        public static double[] RunOLSRegression(MLContext mlContext, IDataView trainingData, IDataView testingData,
+            string labelName, string featuresName = "Features")
         {
-            var featuresArray = GetFeaturesArray(trainingData, labelName);
-            var preprocessingPipeline = mlContext.Transforms.Concatenate("Features", featuresArray);
-            var preprocessedTrainingData = preprocessingPipeline.Fit(trainingData).Transform(trainingData);
-            var preprocessedTestingData = preprocessingPipeline.Fit(trainingData).Transform(testingData);
-
             OlsTrainer.Options options = new OlsTrainer.Options();
             options.LabelColumnName = labelName;
-            options.FeatureColumnName = "Features";
+            options.FeatureColumnName = featuresName;
 
             var trainer = mlContext.Regression.Trainers.Ols(options);
 
-            ITransformer model = trainer.Fit(preprocessedTrainingData);
+            ITransformer model = trainer.Fit(trainingData);
 
-            IDataView trainingPredictions = model.Transform(preprocessedTrainingData);
-            var trainingMetrics = mlContext.Regression.Evaluate(trainingPredictions, labelColumnName: labelName);
-            IDataView testingPredictions = model.Transform(preprocessedTestingData);
-            var testingMetrics = mlContext.Regression.Evaluate(testingPredictions, labelColumnName: labelName);
-
-            double[] metrics = new double[4];
-            metrics[0] = trainingMetrics.RootMeanSquaredError;
-            metrics[1] = testingMetrics.RootMeanSquaredError;
-            metrics[2] = trainingMetrics.RSquared;
-            metrics[3] = testingMetrics.RSquared;
-            return metrics;
+            return EvaluateRegression(mlContext, model, trainingData, testingData, labelName);
         }
 
         static void Main(string[] args)
         {
             // args[0] - training data filename
             // args[1] - testing data filename
-            // args[2] - machine learning task (regression, binary)
-            // args[3] - machine learning algorithm (RandomForest, OLS)
+            // args[2] - label name
+            // args[3] - machine learning task (regression, binary)
+            // args[4] - machine learning algorithm (RandomForest, OLS, LR)
             // Random Forest parameters:
-            //     args[4] - NumberOfTrees
-            //     args[5] - NumberOfLeaves
+            //     args[5] - NumberOfTrees
+            //     args[6] - NumberOfLeaves
+            // Logistic Regression parameters:
+            //     args[5] - Number of iterations
+
             var mlContext = new MLContext(seed: 42);
             // data[0] - training subset
             // data[1] - testing subset
-            IDataView[] data = LoadData(mlContext, args[0], args[1], args[2]);
-            string labelName = "target";
+            string labelName = args[2];
+            IDataView[] data = PreprocessData(mlContext, args[0], args[1], args[3], labelName);
 
             var mainWatch = System.Diagnostics.Stopwatch.StartNew();
             double[] metrics;
-            if (args[3] == "RandomForest")
+            if (args[4] == "RF")
             {
-                int numberOfTrees = Int32.Parse(args[4]);
-                int numberOfLeaves = Int32.Parse(args[5]);
-                if (args[2] == "binary")
+                int numberOfTrees = Int32.Parse(args[5]);
+                int numberOfLeaves = Int32.Parse(args[6]);
+                if (args[3] == "binary")
                 {
 
-                    metrics = RunRandomForestClassification(mlContext, data[0], data[1], labelName, numberOfTrees, numberOfLeaves);
+                    metrics = RunRandomForestClassification(mlContext, data[0], data[1], numberOfTrees, numberOfLeaves, labelName);
                     mainWatch.Stop();
-                    Console.WriteLine("algorithm,all workflow time[ms],training accuracy,testing accuracy,training F1 score,testing F1 score");
+                    Console.WriteLine("algorithm,workload time[ms],training accuracy,testing accuracy,training F1 score,testing F1 score");
                     Console.WriteLine($"Random Forest Binary,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
                 }
                 else
                 {
-                    metrics = RunRandomForestRegression(mlContext, data[0], data[1], labelName, numberOfTrees, numberOfLeaves);
+                    metrics = RunRandomForestRegression(mlContext, data[0], data[1], numberOfTrees, numberOfLeaves, labelName);
                     mainWatch.Stop();
-                    Console.WriteLine("algorithm,all workflow time[ms],training RMSE,testing RMSE,training R2 score,testing R2 score");
+                    Console.WriteLine("algorithm,workload time[ms],training RMSE,testing RMSE,training R2 score,testing R2 score");
                     Console.WriteLine($"Random Forest Regression,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
                 }
             }
-            else if (args[3] == "OLS")
+            else if (args[4] == "OLS")
             {
                 metrics = RunOLSRegression(mlContext, data[0], data[1], labelName);
                 mainWatch.Stop();
-                Console.WriteLine("algorithm,all workflow time[ms],training RMSE,testing RMSE,training R2 score,testing R2 score");
+                Console.WriteLine("algorithm,workload time[ms],training RMSE,testing RMSE,training R2 score,testing R2 score");
                 Console.WriteLine($"OLS Regression,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
+            }
+            else if (args[4] == "LR")
+            {
+                int numberOfIterations = Int32.Parse(args[5]);
+                metrics = RunLogisticRegression(mlContext, data[0], data[1], numberOfIterations, labelName);
+                mainWatch.Stop();
+                Console.WriteLine("algorithm,workload time[ms],training accuracy,testing accuracy,training F1 score,testing F1 score");
+                Console.WriteLine($"Logistic Regression,{mainWatch.Elapsed.TotalMilliseconds},{metrics[0]},{metrics[1]},{metrics[2]},{metrics[3]}");
             }
         }
     }
